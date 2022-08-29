5d7901c44db3ed925e78ec5a674a7b04062d865c
nicohsfu <marcnicohernandez@gmail.com>
2022-08-29 14:26:01 -0700

    isOdd function

---
+++ odd.js
@@ -0,0 +1,9 @@
+function isOdd(num) {
+  return num % 2 === 1;
+}
+
+console.log (isOdd(3));
+
+// pasting this from the instructions to "test" if my code works properly
+console.log("3 is odd: " + isOdd(3));
+console.log("8 is odd: " + isOdd(8));
